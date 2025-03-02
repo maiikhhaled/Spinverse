16baef3998a1ee1c28b15fd01521cc73b3c1f18c
maiikhhaled <120672203+maiikhhaled@users.noreply.github.com>
2025-03-02 18:42:06 +0200

    Initial Commit

---
+++ Diciee/DicieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicieeApp.swift
+//  Diciee
+//
+//  Created by mai khaled on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DicieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
